9bbe0a2dc979bf1ca8f07ca30f68818b63d9e988
juan pablo bustamante <bustamante.jpm@gmail.com>
2023-06-11 16:31:50 -0300

    biome enum, game class with the biomes

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,34 +1,13 @@
 package model;
 
-import model.biomas.Bioma;
+import model.biomes.Biome;
 
 public class Game {
-    private Bioma forest;
-    private Bioma beach;
-    private Bioma cave;
-    private Bioma volcano;
-    private Bioma mountain;
+    Biome forest = Biome.FOREST;
+    Biome mountain = Biome.MOUNTAIN;
+    Biome beach = Biome.BEACH;
+    Biome cave = Biome.CAVE;
+    Biome volcano = Biome.VOLCANO;
 
-    public Game(){
-        //types forest
-        this.forest.addType("Electric");
-        this.forest.addType("Grass");
-        this.forest.addType("Normal");
-        this.forest.addType("Bug");
-        //types beach
-        this.beach.addType("Water");
-        this.beach.addType("Poison");
-        //types cave
-        this.cave.addType("Poison");
-        this.cave.addType("Ground");
-
-        //types volcan
-        this.volcano.addType("Fire");
-        this.volcano.addType("Flying");
-
-        //types mountain
-        this.mountain.addType("Ground");
-        this.mountain.addType("flying");
-    }
 
 }
